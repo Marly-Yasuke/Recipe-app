5cded4dbd66308cf40407622f6e70b77afbfcb9d
Marly Diallo <marly.diallo2021@gmail.com>
2023-06-28 20:23:54 +0200

    fetching to fix

--- recipe/src/components/Popular.jsx
+++ recipe/src/components/Popular.jsx
@@ -1,17 +1,20 @@
+import { useEffect } from "react";
 
+function Popular() {
+  useEffect(() => {
+    getPopular();
+    // the empty array is to make sure to only run it when the component is mounted
+  }, []);
 
-function Popular (){
+  const getPopular = async () => {
+    // need to fix fetching error (check key)
+    const api = await fetch(`
+  https://api.edamam.com/api/recipes/v2?apiKey=${process.env.REACT_APP_API_KEY}&number=10`);
+    const data = await api.json();
+    console.log(data);
+  };
 
-const getPopular = async () => {
-  const api = await fetch(`
-  https://api.edamam.com/api/recipes/v2?apiKey=${process.env.REACT_APP_API_KEY}&number=10`)
-  const data = await api.json()
-  console.log(data)
+  return <div>Popular</div>;
 }
 
-  return (
-    <div>Popular</div>
-  )
-}
-
-export default Popular
+export default Popular;
