9de46ce1ff45e33e50de89d4f3a94312f3fb66ba
Marly Diallo <marly.diallo2021@gmail.com>
2023-07-15 13:02:01 +0200

    useState to get search result

--- recipe/src/components/Popular.jsx
+++ recipe/src/components/Popular.jsx
@@ -1,19 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Popular() {
+  const [popular, setPopular] = useState([])
   useEffect(() => {
     getPopular();
     // the empty array is to make sure to only run it when the component is mounted
   }, []);
 
   const getPopular = async () => {
+    // need to fix fetching error (check key)
     const api = await fetch(`
     https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10`);
     const data = await api.json();
-    console.log(data);
+setPopular(data.recipes)
   };
 
-  return <div>Popular</div>;
+  return <div>{popular.map((recipe) => {
+    return (
+<div>
+  <p>{recipe.title}</p>
+</div>
+    )
+  })}</div>;
 }
 
 export default Popular;
