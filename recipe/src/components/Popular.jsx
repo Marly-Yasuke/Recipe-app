7384c3963546b41e317aafdfd1d3f1cc83e1e746
Marly Diallo <marly.diallo2021@gmail.com>
2023-07-17 20:52:54 +0200

    update linear gradient

--- recipe/src/components/Popular.jsx
+++ recipe/src/components/Popular.jsx
@@ -38,6 +38,7 @@ function Popular() {
                 <Card key={recipe.id}>
                   <p>{recipe.title}</p>
                   <img src={recipe.image} alt={recipe.title} />
+                  <Gradient/>
                 </Card>
               </SplideSlide>
             );
@@ -83,5 +84,13 @@ const Card = styled.div`
   align-items: center;
   }
 `;
+
+const Gradient = styled.div`
+z-index: 3;
+position: absoulute;
+width: 100%,
+height: 100%;
+background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+`
 
 export default Popular;
