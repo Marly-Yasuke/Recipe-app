3a2a14c04f680286c07be1cbada4c770f31c3d86
Marly Diallo <marly.diallo2021@gmail.com>
2023-08-03 19:55:48 +0200

    Category.jsx imports setup

--- recipe/src/components/Category.jsx
+++ recipe/src/components/Category.jsx
@@ -1,2 +1,14 @@
-import {FaPizzaSlice, FaHamburger} from 'react-icons/fa';
+import {FaPizzaSlice, FaHamburger, FaRegFileCode} from 'react-icons/fa';
 import {GiNoodles, GiChopSticks} from 'react-icons/gi'
+import styled from "styled-components";
+import { NavLink } from 'react-router-dom';
+
+function Category(){
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Category
