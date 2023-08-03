83d41aa39ad9db53283a02942455fee1b96b3ecf
Marly Diallo <marly.diallo2021@gmail.com>
2023-08-03 20:25:29 +0200

    Navlink + routing

--- recipe/src/pages/Pages.jsx
+++ recipe/src/pages/Pages.jsx
@@ -1,15 +1,14 @@
 import Home from "./Home"
-import {Route, Routes, BrowserRouter} from 'react-router-dom'
+import {Route, Routes} from 'react-router-dom'
 import Cuisine from "./Cuisine"
 
 function Pages (){
   return (
-    <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/cuisine" element={<Cuisine/>}/>
     </Routes>
-    </BrowserRouter>
+
   )
 }
 
