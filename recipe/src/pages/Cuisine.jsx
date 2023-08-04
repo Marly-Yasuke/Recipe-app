afa0aa007fe6b13713ae2c70437cc071adc9094e
Marly Diallo <marly.diallo2021@gmail.com>
2023-08-04 16:27:46 +0200

    cuisine fetch

--- recipe/src/pages/Cuisine.jsx
+++ recipe/src/pages/Cuisine.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Styled } from "styled-components";
+import { motion } from "framer-motion";
+import { Link, useParams } from "react-router-dom";
+import { useState } from "react";
 
 function Cuisine() {
+  const [cuisine, setCuisine] = useState([]);
+
+  const getCuisine = async (name) => {
+    const data = await fetch( `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}&number=10`
+    )
+    const recipes = await data.json();
+    setCuisine(recipes.results)
+  }
+
+  useEffect(()=>{
+getCuisine('italian')
+  },[])
   return <div>Cuisine</div>;
 }
 
